5cec8dfd0808676d3fb51d79a16ba6ea578c4de2
croschel <croschel000@gmail.com>
2018-06-26 12:58:09 -0300

    Testing database loja

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,6 +3,13 @@
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
     ?>
+    
+    <?php 
+    $query = "insert into produtos(nome,preco) values('{$nome}',{$preco})";
+    $conexao = mysqli_connect('localhost','root','',loja);
+    ?>
     <p>Produto <?php echo $nome; ?> com preço igual à <?php echo $preco;?> foi adicionado com sucessso! </p>
+    
+    
     
 <?php include("rodape.php")?>
