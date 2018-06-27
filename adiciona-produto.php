d069caa3c55df39f13f6c6a4915e5ab8fb034c96
croschel <36478774+croschel@users.noreply.github.com>
2018-06-26 21:40:04 -0300

    Connection with Database is done

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,4 +1,5 @@
-<?php include("cabecalho.php")?>
+    <?php include("cabecalho.php")?>
+
     <?php 
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
@@ -6,10 +7,19 @@
     
     <?php 
     $query = "insert into produtos(nome,preco) values('{$nome}',{$preco})";
-    $conexao = mysqli_connect('localhost','root','',loja);
+    $conexao = mysqli_connect('localhost','root','','loja');
+
+    if(mysqli_query($conexao, $query)){ //mysqli é o novo pacote de gerencia dentro do php?>
+
+    <p class="alert-success">Produto <?php echo $nome; ?> com preço igual à <?php echo $preco;?> foi adicionado com sucessso! </p>
+    
+    <?php 
+    }else{?>
+    
+    <p class="alert-danger">O produto<? = $nome?> não foi adicionado</p>
+
+    <?php 
+    }
     ?>
-    <p>Produto <?php echo $nome; ?> com preço igual à <?php echo $preco;?> foi adicionado com sucessso! </p>
-    
-    
-    
-<?php include("rodape.php")?>
+
+    <?php include("rodape.php")?>
