86f8d221187f989cb757ea375f3bcdf5262acd28
croschel <croschel000@gmail.com>
2018-07-18 15:39:53 -0300

    Playing with checkbox and select tag

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -7,9 +7,14 @@
     $preco = $_POST["preco"];
     $descricao = $_POST['descricao'];
     $categoria_id = $_POST['categoria_id'];
+    if(array_key_exists('usado',$_POST)){
+        $usado = "true";
+    }else{
+        $usado = "false";
+    }
     //variaveis
 
-    if(insereProduto($conexao,$nome,$preco,$descricao,$categoria_id)){ //mysqli é o novo pacote de gerência de DB dentro do php?>
+    if(insereProduto($conexao,$nome,$preco,$descricao,$categoria_id,$usado)){ //mysqli é o novo pacote de gerência de DB dentro do php?>
 
     <p class="text-success">Produto <?php echo $nome; ?> com preço igual à <?php echo $preco;?> foi adicionado com sucessso! </p>
     
@@ -17,7 +22,7 @@
     }else{
         $msg = mysqli_error($conexao);    
     ?>
-    <p class="alert-danger">O produto<?= $nome?> não foi adicionado <? $msg ?></p> 
+    <p class="alert-danger">O produto <?= $nome?> não foi adicionado <? $msg ?></p> 
 
     <?php 
     }
--- produto-formulario.php
+++ produto-formulario.php
@@ -8,35 +8,43 @@
     <h1>Inserir Produto</h1>
     
         <form action="adiciona-produto.php" method = "post">
-            <table>
-                <tr>
-                <td><label for="nome">Nome</label></td>
-                <td><input type="text" name="nome" class="form-control"/></td>
-                </tr>
+            <table class="table">
+                <tbody> 
+                    <tr>
+                    <td><label for="nome">Nome</label></td>
+                    <td><input type="text" name="nome" class="form-control"/></td>
+                    </tr>
 
-                <tr>
-                <td><label for="preco">Preço</label></td>
-                <td><input type="number" name="preco" class="form-control"/></td>
-                </tr>
+                    <tr>
+                    <td><label for="preco">Preço</label></td>
+                    <td><input type="number" name="preco" class="form-control"/></td>
+                    </tr>
 
-                 <tr>
-                <td><label for="descricao">Descrição</label></td>
-                <td><textarea name="descricao" class="form-control"></textarea></td>
-                </tr>
+                    <tr>
+                    <td><label for="descricao">Descrição</label></td>
+                    <td><textarea name="descricao" class="form-control"></textarea></td>
+                    </tr>
 
-                <tr>
-                <td><label for="categoria_id">Descrição</label></td>
-                <td>
-                <?php foreach($categorias as $categoria) :?>
-                <input type="radio" name="categoria_id" value="<?=$categoria['id'];?>"><?=$categoria['nome'];?></br>
-                <?php endforeach;?>
-                </td>
-                </tr>
+                    <tr>
+                    <td><label for="usado"></label></td>
+                    <td><input type="checkbox" name="usado" value="true">Usado</td>
+                    </tr>
 
-                <tr>
-                <td><input type="submit" value="Cadastrar" class="btn btn-primary"/></td>
-                </tr>
+                    <tr>
+                    <td><label for="categoria_id">Categoria</label></td>
+                    <td>
+                    <select name="categoria_id" class="form-control">
+                        <?php foreach($categorias as $categoria) :?>
+                    <option value="<?=$categoria['id'];?>"><?=$categoria['nome'];?></option>
+                        <?php endforeach;?>
+                    </select>
+                    </td>
+                    </tr>
 
+                    <tr>
+                    <td><input type="submit" value="Cadastrar" class="btn btn-primary"/></td>
+                    </tr>
+                </tbody>
 
             </table>
         </form>
